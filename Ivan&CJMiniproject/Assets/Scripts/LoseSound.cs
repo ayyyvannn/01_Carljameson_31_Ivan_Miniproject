7cc22ccde66ccf8615c8447bdec0da086229bc7b
DcPz <64124360+DcPz@users.noreply.github.com>
2020-08-24 17:20:01 +0800

    Updated Win and Lose Screen

---
+++ Ivan&CJMiniproject/Assets/Scripts/LoseSound.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LoseSound : MonoBehaviour
+{
+    public AudioSource Lose;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Lose.Play();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Ivan&CJMiniproject/Assets/Scripts/PlaySound.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlaySound : MonoBehaviour
+{
+    public AudioSource Win;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Win.Play();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
